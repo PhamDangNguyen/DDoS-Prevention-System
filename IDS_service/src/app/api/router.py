9305eac23ad29087278c2bf1f379559d6556b906
Doanhdz <nguyenduyanh821998@gmail.com>
2025-10-03 01:13:24 +0700

    add test tool cicflowmeter

--- IDS_service/src/app/api/router.py
+++ IDS_service/src/app/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from typing import Optional
-from src.services.dto import InputRequestNSLKDD, Response, InputRequestCICIDS2017
+from src.services.dto import InputRequestNSLKDD, Response, InputRequestCICIDS2017, SelectedFeaturetCICIDS2017
 from src.services.anomaly import AnomalyService
 from src.services.blocks import ColumnOrderer
 
@@ -25,7 +25,13 @@ def check_anomaly(input_request:InputRequestNSLKDD):
 def check_anomaly(input_request:InputRequestCICIDS2017):
     try:
         anomalyService = AnomalyService(data_name="CIC-IDS2017")
-        pred = anomalyService.infer(input_request)
+        # Create a dictionary of ModelA fields
+        input_request_dict = input_request.dict()
+        # Get only the fields that are also in ModelB
+        select_feat_dict = {key: input_request_dict[key] for key in input_request_dict if key in SelectedFeaturetCICIDS2017.__annotations__}
+        # Create ModelB instance using the filtered fields
+        model_b_instance = SelectedFeaturetCICIDS2017(**select_feat_dict)
+        pred = anomalyService.infer(model_b_instance)
         result = {
             "status_code": 200,
             "result": pred.get("prediction", "normal"),
--- IDS_service/src/services/dto.py
+++ IDS_service/src/services/dto.py
@@ -18,45 +18,131 @@ class InputRequestNSLKDD(BaseModel):
     srv_serror_rate: Union[int, float]
     rerror_rate: Union[int, float]
 
+class SelectedFeaturetCICIDS2017(BaseModel):
+    psh_flag_cnt: Union[str, int, float]
+    fwd_seg_size_min: Union[str, int, float]
+    flow_iat_max: Union[str, int, float]
+    flow_iat_min: Union[str, int, float]
+    ack_flag_cnt: Union[str, int, float]
+    dst_port: Union[str, int, float]
+    bwd_pkt_len_mean: Union[str, int, float]
+    bwd_pkt_len_max: Union[str, int, float]
+    bwd_pkt_len_min: Union[str, int, float]
+    init_fwd_win_byts: Union[str, int, float]
+    fwd_iat_max: Union[str, int, float]
+    idle_mean: Union[str, int, float]
+    idle_max: Union[str, int, float]
+    bwd_seg_size_avg: Union[str, int, float]
+    bwd_pkt_len_std: Union[str, int, float]
+    bwd_iat_mean: Union[str, int, float]
+    fwd_iat_std: Union[str, int, float]
+    down_up_ratio: Union[str, int, float]
+    pkt_len_max: Union[str, int, float]
+    pkt_size_avg: Union[str, int, float]
+    pkt_len_min: Union[str, int, float]
+    pkt_len_std: Union[str, int, float]
+    fwd_pkts_s: Union[str, int, float]
+    pkt_len_mean: Union[str, int, float]
+    flow_iat_std: Union[str, int, float]
+    urg_flag_cnt: Union[str, int, float]
+    fin_flag_cnt: Union[str, int, float]
+    fwd_pkt_len_min: Union[str, int, float]
+    subflow_fwd_pkts: Union[str, int, float]
+    bwd_iat_max: Union[str, int, float]
+    pkt_len_var: Union[str, int, float]
+    fwd_iat_mean: Union[str, int, float]
+    flow_duration: Union[str, int, float]
+    fwd_iat_tot: Union[str, int, float]
+    bwd_iat_std: Union[str, int, float]
+    flow_iat_mean: Union[str, int, float]
+
 class InputRequestCICIDS2017(BaseModel):
-    psh_flag_count: int
-    min_seg_size_forward: int
-    flow_iat_max: int
-    flow_iat_min: int
-    ack_flag_count: int
-    destination_port: int
-    bwd_packet_length_mean: float
-    bwd_packet_length_max: int
-    bwd_packet_length_min: int
-    init_win_bytes_forward: int
-    fwd_iat_max: int
-    idle_mean: float
-    idle_max: int
-    avg_bwd_segment_size: float
-    bwd_packet_length_std: float
-    bwd_iat_mean: float
-    fwd_iat_std: float
-    down_up_ratio: int
-    max_packet_length: int
-    average_packet_size: float
-    min_packet_length: int
-    packet_length_std: float
-    fwd_packets_s: float
-    packet_length_mean: float
-    flow_iat_std: float
-    urg_flag_count: int
-    fin_flag_count: int
-    fwd_packet_length_min: int
-    subflow_fwd_packets: int
-    bwd_iat_max: int
-    packet_length_variance: float
-    fwd_iat_mean: float
-    flow_duration: int
-    fwd_iat_total: int
-    bwd_iat_std: float
-    flow_iat_mean: float
+    src_ip: Union[str, int, float]
+    dst_ip: Union[str, int, float]
+    src_port: Union[str, int, float]
+    dst_port: Union[str, int, float]
+    protocol: Union[str, int, float]
+    timestamp: Union[str, int, float]
+    flow_duration: Union[str, int, float]
+    flow_byts_s: Union[str, int, float]
+    flow_pkts_s: Union[str, int, float]
+    fwd_pkts_s: Union[str, int, float]
+    bwd_pkts_s: Union[str, int, float]
+    tot_fwd_pkts: Union[str, int, float]
+    tot_bwd_pkts: Union[str, int, float]
+    totlen_fwd_pkts: Union[str, int, float]
+    totlen_bwd_pkts: Union[str, int, float]
+    fwd_pkt_len_max: Union[str, int, float]
+    fwd_pkt_len_min: Union[str, int, float]
+    fwd_pkt_len_mean: Union[str, int, float]
+    fwd_pkt_len_std: Union[str, int, float]
+    bwd_pkt_len_max: Union[str, int, float]
+    bwd_pkt_len_min: Union[str, int, float]
+    bwd_pkt_len_mean: Union[str, int, float]
+    bwd_pkt_len_std: Union[str, int, float]
+    pkt_len_max: Union[str, int, float]
+    pkt_len_min: Union[str, int, float]
+    pkt_len_mean: Union[str, int, float]
+    pkt_len_std: Union[str, int, float]
+    pkt_len_var: Union[str, int, float]
+    fwd_header_len: Union[str, int, float]
+    bwd_header_len: Union[str, int, float]
+    fwd_seg_size_min: Union[str, int, float]
+    fwd_act_data_pkts: Union[str, int, float]
+    flow_iat_mean: Union[str, int, float]
+    flow_iat_max: Union[str, int, float]
+    flow_iat_min: Union[str, int, float]
+    flow_iat_std: Union[str, int, float]
+    fwd_iat_tot: Union[str, int, float]
+    fwd_iat_max: Union[str, int, float]
+    fwd_iat_min: Union[str, int, float]
+    fwd_iat_mean: Union[str, int, float]
+    fwd_iat_std: Union[str, int, float]
+    bwd_iat_tot: Union[str, int, float]
+    bwd_iat_max: Union[str, int, float]
+    bwd_iat_min: Union[str, int, float]
+    bwd_iat_mean: Union[str, int, float]
+    bwd_iat_std: Union[str, int, float]
+    fwd_psh_flags: Union[str, int, float]
+    bwd_psh_flags: Union[str, int, float]
+    fwd_urg_flags: Union[str, int, float]
+    bwd_urg_flags: Union[str, int, float]
+    fin_flag_cnt: Union[str, int, float]
+    syn_flag_cnt: Union[str, int, float]
+    rst_flag_cnt: Union[str, int, float]
+    psh_flag_cnt: Union[str, int, float]
+    ack_flag_cnt: Union[str, int, float]
+    urg_flag_cnt: Union[str, int, float]
+    ece_flag_cnt: Union[str, int, float]
+    down_up_ratio: Union[str, int, float]
+    pkt_size_avg: Union[str, int, float]
+    init_fwd_win_byts: Union[str, int, float]
+    init_bwd_win_byts: Union[str, int, float]
+    active_max: Union[str, int, float]
+    active_min: Union[str, int, float]
+    active_mean: Union[str, int, float]
+    active_std: Union[str, int, float]
+    idle_max: Union[str, int, float]
+    idle_min: Union[str, int, float]
+    idle_mean: Union[str, int, float]
+    idle_std: Union[str, int, float]
+    fwd_byts_b_avg: Union[str, int, float]
+    fwd_pkts_b_avg: Union[str, int, float]
+    bwd_byts_b_avg: Union[str, int, float]
+    bwd_pkts_b_avg: Union[str, int, float]
+    fwd_blk_rate_avg: Union[str, int, float]
+    bwd_blk_rate_avg: Union[str, int, float]
+    fwd_seg_size_avg: Union[str, int, float]
+    bwd_seg_size_avg: Union[str, int, float]
+    cwe_flag_count: Union[str, int, float]
+    subflow_fwd_pkts: Union[str, int, float]
+    subflow_bwd_pkts: Union[str, int, float]
+    subflow_fwd_byts: Union[str, int, float]
+    subflow_bwd_byts: Union[str, int, float]
 
 class Response(BaseModel):
     status_code: int = 200
     result: str = "normal"
     confidence: Union[float, int, dict, str] = None
+
+
